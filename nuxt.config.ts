75cebc88793ba70433544b34cc80b2267aba0e91
3446798488 <3446798488@qq.com>
2024-05-26 03:16:02 +0000

    feat: 完成 nuxt 版本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["~/modules/font"],
+  modules: ["node_modules/vite-plugin-font/src/nuxt"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div
-    :style="{
-      'font-family': `'${css.family}'`,
-    }"
+    :style='{
+      "font-family":`"${css.family}"`
+    }'
   >
     中文网字计划
   </div>
 </template>
 <script setup>
-import { css } from "../assets/Hangeuljaemin4-Regular.otf";
+import {css} from "~/assets/Hangeuljaemin4-Regular.otf";
+console.log(css)
 </script>
