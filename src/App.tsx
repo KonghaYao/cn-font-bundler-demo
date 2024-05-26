e7e95d1724d2bd74c9da37273019e88b004ba66e
3446798488 <3446798488@qq.com>
2024-05-26 05:38:01 +0000

    feat: rspack 支持

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import {css} from '../assets/Hangeuljaemin4-Regular.otf'
+function App() {
+	const [count, setCount] = useState(0);
+	console.log(css);
+	return (
+		<div className="App" style={{
+			fontFamily: `'${css.family}'`
+		}}>
+			<div>
+				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Rspack + React + TypeScript</h1>
+			<div className="card">
+				<button onClick={() => setCount(count => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Rspack and React logos to learn more
+			</p>
+		</div>
+	);
+}
+
+export default App;
