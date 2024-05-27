60537cf7bed2b9ac37c082c09037ab350db8267e
3446798488 <3446798488@qq.com>
2024-05-27 03:19:46 +0000

    feat: 支持 astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import font from 'vite-plugin-font'
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [font()],
+  },
+});
