73b407c3601f30493a848ea87f138a9e257b2bd5
3446798488 <3446798488@qq.com>
2024-08-07 01:13:06 +0000

    chore: 2.0.0

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 import font from 'vite-plugin-font'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),font()],
+  plugins: [vue(),font.vite()],
 })
