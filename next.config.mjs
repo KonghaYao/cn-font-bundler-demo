b38adcd7c499c18751f359aeae1bb812b5d6bfa8
3446798488 <3446798488@qq.com>
2024-08-07 01:28:08 +0000

    chore: 2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
+import font from 'vite-plugin-font'
 const nextConfig = {
     webpack: (config, options) => {
-        config.module.rules.push({
-            test: /\.otf/,
-            use: [
-                {
-                    loader: './node_modules/vite-plugin-font/dist/webpack.mjs',
-                    options: {
-                    },
-                },
-            ],
-        })
+        config.plugins.push(font.webpack())
         return config
     },
 };
