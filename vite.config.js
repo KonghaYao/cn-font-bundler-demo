6e5c34dfbec1330f9751d9bcb55262474b4a4b84
3446798488 <3446798488@qq.com>
2024-08-09 14:29:07 +0000

    feat: 修复 vite 构建

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 import font from 'vite-plugin-font'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),font.vite()],
+  plugins: [vue(), font.vite()],
 })
