1c371de6ccfd3efa1e062d0b32e38bb02835ac3f
3446798488 <3446798488@qq.com>
2024-08-09 13:43:47 +0000

    feat: nuxt 插件解决

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["node_modules/vite-plugin-font/src/nuxt"],
+  modules: ["node_modules/vite-plugin-font/dist/nuxt"],
   fontSplit: {
     scanFiles: ["pages/**.vue"],
   },
